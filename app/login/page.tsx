fc36878f1346d98427b92c0f2d3a0ffcd49d5c77
Smanikanta21 <siraparapuabhinay21@gmail.com>
2025-08-17 14:42:49 +0530

    added use state fixed minor bugs

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,29 +1,31 @@
 "use client";
-import {X,Eye,EyeClosed } from "lucide-react";
-import React from "react";
-import { useState } from "react";
-import { useRouter } from "next/navigation"
+import { X, Eye, EyeClosed } from "lucide-react";
+import React, { useState } from "react";
+
 type PropData = {
-  setShowLogin?: (show: boolean) => void;
+    setShowLogin?: (show: boolean) => void;
 };
 
 export default function LoginPage({ setShowLogin }: PropData) {
     const [showPassword, setShowPassword] = useState(false);
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
     return (
         <>
-            <div className="fixed top-6 left-4 hover:cursor-pointer hover:scale-120 ease-in-out duration-150" onClick={()=> setShowLogin && setShowLogin(false)}><X/></div>
+            <div className="fixed top-6 left-4 hover:cursor-pointer hover:scale-125 ease-in-out duration-150" onClick={() => setShowLogin && setShowLogin(false)}><X /></div>
             <div className="flex items-center justify-center h-screen bg-transparent">
                 <div className="bg-white/20 backdrop-blur-lg p-8 rounded-lg shadow-lg w-full max-w-md">
                     <h1 className="text-2xl font-bold text-center text-white mb-6">Login</h1>
                     <form>
                         <div className="mb-4">
                             <label className="block text-white mb-2" htmlFor="email">Email</label>
-                            <input type="email" id="email" placeholder="Enter Your Email" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
+                            <input type="email" id="email" placeholder="Enter Your Email" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e)=>{setEmail(e.target.value)}}/>
                         </div>
-                        <div className="mb-6">
+                        <div className="mb-6 relative">
                             <label className="block text-white mb-2" htmlFor="password">Password</label>
-                            <input type={showPassword? "text":"password"} id="password" placeholder="Enter Your Password" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 relative" required />
-                            <button className="absolute right-12 top-55 bottom-50" onClick={()=>{setShowPassword(!showPassword)}}>{showPassword ? <Eye/> : <EyeClosed/>}</button>
+                            <input type={showPassword ? "text" : "password"} id="password" placeholder="Enter Your Password" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e)=>{setPassword(e.target.value)}}/>
+                            <button type="button" className="absolute right-3 top-[38px]" onClick={() => setShowPassword(!showPassword)}>{showPassword ? <Eye /> : <EyeClosed />}</button>
                         </div>
                         <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-200">Login</button>
                     </form>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,9 +7,9 @@ type PropData = {
 };
 export default function SignupPage({ showSignup, setShowSignup }: PropData) {
 
-    const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState<string>("");
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
     const [showPassword, setShowPassword] = useState(false);
     return (
@@ -21,15 +21,15 @@ export default function SignupPage({ showSignup, setShowSignup }: PropData) {
                 <form>
                     <div className="mb-4">
                         <label className="block text-white mb-2" htmlFor="email">Email</label>
-                        <input type="email" placeholder="Enter Your Email" id="email" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
+                        <input type="email" placeholder="Enter Your Email" id="email" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e)=>{setEmail(e.target.value)}}/>
                     </div>
                     <div className="mb-4">
                         <label className="block text-white mb-2" htmlFor="username">Username</label>
-                        <input type="text" id="username" placeholder="Enter Your Username" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
+                        <input type="text" id="username" placeholder="Enter Your Username" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e)=>{setUsername(e.target.value)}}/>
                     </div>
                     <div className="mb-6">
                         <label className="block text-white mb-2" htmlFor="password">Password</label>
-                        <input type={showPassword?"text":"password"} id="password" placeholder="Enter Your Password" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 relative" required />
+                        <input type={showPassword?"text":"password"} id="password" placeholder="Enter Your Password" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 relative" required onChange={(e)=>{setPassword(e.target.value)}}/>
                         <button className="absolute right-12 top-77 bottom-50" onClick={(e)=>{setShowPassword(!showPassword)}}>{showPassword ? <Eye/> : <EyeClosed/>}</button>
                     </div>
                     <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-200">Sign Up</button>
