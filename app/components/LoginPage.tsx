31547f75d53111996a033ec63a83001caacc4270
Smanikanta21 <siraparapuabhinay21@gmail.com>
2025-08-17 16:41:30 +0530

    changed login signup page routes

--- app/components/LoginPage.tsx
+++ app/components/LoginPage.tsx
@@ -19,7 +19,7 @@ export default function LoginPage({ setShowLogin, setShowSignup }: PropData) {
     const handleLogin = async(e:React.FormEvent)=>{
         e.preventDefault();
         try{
-            const res = await fetch('/api/login', {
+            const res = await fetch('/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }),
