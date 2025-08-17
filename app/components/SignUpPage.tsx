19916696ebcc9f5627ca304cb732a88429c8c674
Smanikanta21 <siraparapuabhinay21@gmail.com>
2025-08-17 19:04:14 +0530

    fixed logging and account creating issues

--- app/components/SignUpPage.tsx
+++ app/components/SignUpPage.tsx
@@ -17,7 +17,7 @@ export default function SignupPage({ setShowSignup, setShowLogin }: PropData) {
   const handleSignUp = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await fetch("https://www.syncbeats.app/signup", {
+      const res = await fetch("/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, username, password }),
