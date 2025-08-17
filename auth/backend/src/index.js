48611e0bc9ca96160a8fe8af7ef459303c547961
Smanikanta21 <siraparapuabhinay21@gmail.com>
2025-08-17 19:45:40 +0530

    changed

--- auth/backend/src/index.js
+++ auth/backend/src/index.js
@@ -6,15 +6,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+
 app.use(cors({
   origin: ["https://www.syncbeats.app", "http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
 
-app.options("*", cors());
 app.use(express.json());
-app.use("https://www.syncbeats.app/auth", authRouter);
+app.use("/auth", authRouter);
 
 app.get('/', (req, res) => {
   res.send('Auth is running');
