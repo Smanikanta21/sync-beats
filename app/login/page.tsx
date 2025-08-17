8a6a871fcdb3910396b1f99fc5dcdaf6bb4ac2be
Smanikanta21 <siraparapuabhinay21@gmail.com>
2025-08-17 14:08:50 +0530

    unused imports removed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -29,7 +29,7 @@ export default function LoginPage(props: PropData) {
                         <div className="mb-6">
                             <label className="block text-white mb-2" htmlFor="password">Password</label>
                             <input type={showPassword? "text":"password"} id="password" placeholder="Enter Your Password" className="w-full p-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 relative" required />
-                            <button className="absolute right-12 top-55 bottom-50" onClick={(e)=>{setShowPassword(!showPassword)}}>{showPassword ? <Eye/> : <EyeClosed/>}</button>
+                            <button className="absolute right-12 top-55 bottom-50" onClick={()=>{setShowPassword(!showPassword)}}>{showPassword ? <Eye/> : <EyeClosed/>}</button>
                         </div>
                         <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-200">Login</button>
                     </form>
