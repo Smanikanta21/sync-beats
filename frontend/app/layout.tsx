60290e1f13c4bf735b6f070b77e334427a04a0f3
Smanikanta21 <siraparapuabhinay21@gmail.com>
2025-10-19 12:42:16 +0530

    fix: remove @ts-ignore and add proper CSS type declarations

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+
 });
 
 const geistMono = Geist_Mono({
@@ -19,8 +20,8 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Sync Beats",
   description: "Play Music In Sync with multiple devices at same time",
-    icons: {
-    icon: '/public/image/favicon.svg',  
+  icons: {
+    icon: '/images/favicon.svg',  
   },
 };
 
